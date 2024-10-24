a0da3bdd1c4062766262d48f337c4952befca62f
JavohirShuhratov <shuhratovjavohir2022@gmail.com>
2024-10-24 10:31:03 +0500

    vursion 1Ñ.0.0Ñ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,66 @@
-import Link from 'next/link'
-import InstagramIcon from '@mui/icons-material/Instagram';
 import TelegramIcon from '@mui/icons-material/Telegram';
+import PhoneIcon from '@mui/icons-material/Phone';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export default function RootPage() {
     return (
         <div
             className='h-screen flex justify-center items-center root-bg '
             style={{
-                background: "rgb(251,238,255)",
-                background: "linear-gradient(147deg, rgba(251,238,255,1) 0%, rgba(224,188,252,1) 100%)",
+                background: "linear-gradient(147deg, rgba(251,238,255,1) 0%, rgba(224,188,252,1) 100%)"
             }}
         >
             {/* Content */}
             <div className='w-[450px] h-[400px] text-center flex flex-col gap-5'>
                 {/* Logo */}
-                <span className='font-bold text-white text-3xl'>Logo</span>
-                {/* Subtitle */}
-                <p className='font-medium text-white text-xl'>This is text for чтобы просто хотябы был какой то
-                    текст</p>
+                <span className='font-bold text-3xl text-[rgb(235,159,255)]'>
+                    Malika Studio
+                </span>
                 <ul className="flex flex-col gap-5">
                     <li
                         className='rounded-[8px]'
                         style={{
-                            background: "rgb(235,159,255)",
                             background: "linear-gradient(90deg, rgba(235,159,255,1) 0%, rgba(126,149,252,1) 100%)",
                         }}
                     >
                         <a
-                            className="py-4 text-white flex items-center justify-center gap-2"
-                            href="https://www.instagram.com/shukhratov.j_/" target="_blank" rel="noopener noreferrer"
+                            className="py-4 text-white text-[22px] flex items-center justify-center gap-2"
+                            href="https://www.instagram.com/xina_brow_malika/"
+                            target="_blank"
+                            rel="noopener noreferrer"
                         >
-                            <InstagramIcon style={{ width: '30px', height: '30px' }}/>
-                            <div className='flex flex-col gap-0'>
-                                <h4 className='text-[16px] uppercase'>Instagram</h4>
-                                <p className='text-[14px]'>Тут про наше творчество</p>
-                            </div>
+                            <InstagramIcon style={{width: '30px', height: '30px'}}/>
+                            Instagram
                         </a>
                     </li>
 
                     <li
                         className='rounded-[8px]'
                         style={{
-                            background: "rgb(235,159,255)",
                             background: "linear-gradient(90deg, rgba(235,159,255,1) 0%, rgba(126,149,252,1) 100%)",
                         }}
                     >
                         <a
                             className="py-4 text-white text-[22px] flex items-center justify-center gap-2"
-                            href="https://www.instagram.com/shukhratov.j_/" target="_blank" rel="noopener noreferrer"
+                            href="https://t.me/Shopmalikaa" target="_blank" rel="noopener noreferrer"
                         >
-                            <TelegramIcon style={{ width: '30px', height: '30px' }} />
+                            <TelegramIcon style={{width: '30px', height: '30px'}}/>
                             Telegram
+                        </a>
+                    </li>
+
+                    <li
+                        className='rounded-[8px]'
+                        style={{
+                            background: "linear-gradient(90deg, rgba(235,159,255,1) 0%, rgba(126,149,252,1) 100%)",
+                        }}
+                    >
+                        <a
+                            className="py-4 text-white text-[22px] flex items-center justify-center gap-2"
+                            href="tel:+998999935106" rel="noopener noreferrer"
+                        >
+                            <PhoneIcon style={{width: '30px', height: '30px'}}/>
+                            Call
                         </a>
                     </li>
                 </ul>

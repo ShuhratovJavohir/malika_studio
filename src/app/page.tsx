db6f9cf87d65fb76fbbd4f6dbe89a63b4a38b680
JavohirShuhratov <shuhratovjavohir2022@gmail.com>
2024-10-24 10:37:22 +0500

    responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 export default function RootPage() {
     return (
         <div
-            className='h-screen flex justify-center items-center root-bg '
+            className='min-h-screen flex justify-center items-center root-bg p-4'
             style={{
                 background: "linear-gradient(147deg, rgba(251,238,255,1) 0%, rgba(224,188,252,1) 100%)"
             }}
         >
             {/* Content */}
-            <div className='w-[450px] h-[400px] text-center flex flex-col gap-5'>
+            <div className='w-full max-w-[450px] h-auto text-center flex flex-col gap-5'>
                 {/* Logo */}
                 <span className='font-bold text-3xl text-[rgb(235,159,255)]'>
                     Malika Studio
@@ -24,12 +24,12 @@ export default function RootPage() {
                         }}
                     >
                         <a
-                            className="py-4 text-white text-[22px] flex items-center justify-center gap-2"
+                            className="py-4 text-white text-[20px] md:text-[22px] flex items-center justify-center gap-2"
                             href="https://www.instagram.com/xina_brow_malika/"
                             target="_blank"
                             rel="noopener noreferrer"
                         >
-                            <InstagramIcon style={{width: '30px', height: '30px'}}/>
+                            <InstagramIcon style={{ width: '30px', height: '30px' }} />
                             Instagram
                         </a>
                     </li>
@@ -41,10 +41,10 @@ export default function RootPage() {
                         }}
                     >
                         <a
-                            className="py-4 text-white text-[22px] flex items-center justify-center gap-2"
+                            className="py-4 text-white text-[20px] md:text-[22px] flex items-center justify-center gap-2"
                             href="https://t.me/Shopmalikaa" target="_blank" rel="noopener noreferrer"
                         >
-                            <TelegramIcon style={{width: '30px', height: '30px'}}/>
+                            <TelegramIcon style={{ width: '30px', height: '30px' }} />
                             Telegram
                         </a>
                     </li>
@@ -56,10 +56,10 @@ export default function RootPage() {
                         }}
                     >
                         <a
-                            className="py-4 text-white text-[22px] flex items-center justify-center gap-2"
+                            className="py-4 text-white text-[20px] md:text-[22px] flex items-center justify-center gap-2"
                             href="tel:+998999935106" rel="noopener noreferrer"
                         >
-                            <PhoneIcon style={{width: '30px', height: '30px'}}/>
+                            <PhoneIcon style={{ width: '30px', height: '30px' }} />
                             Call
                         </a>
                     </li>
